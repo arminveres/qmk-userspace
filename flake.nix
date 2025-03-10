c933749e5ee61136b28229c77f2df01c94ad89d1
Armin Veres <armin.veres@hotmail.com>
2025-03-10 18:48:35 +0100

    build: add flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          # overlays = [ (self: super: { gcc-arm-embedded = super.gcc-arm-embedded-13; }) ];
+        };
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs;[
+            zsh
+            qmk
+            cppcheck
+          ];
+        };
+      });
+}
