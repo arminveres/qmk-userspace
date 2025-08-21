bb4096e388c3ee1b3fdd946c90601209694d8e32
Armin Richard Veres <arminrichard.veres@varian.com>
2025-08-21 17:56:32 +0200

    build: add ccache to flake

--- flake.nix
+++ flake.nix
@@ -5,17 +5,14 @@
 
   outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+      let pkgs = import nixpkgs { inherit system; };
+      in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs;[
-            zsh
-            qmk
-            cppcheck
-            nixd
-          ];
+          packages = with pkgs; [ zsh qmk cppcheck nixd ccache ];
+          shellHook = ''
+            export QMK_CCACHE=y
+            export QMK_CONCURRENCY=`nproc`
+          '';
         };
       });
 }
