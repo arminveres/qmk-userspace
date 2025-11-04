7ca346acab239fcca0cf5be10ad46fd7ef1d3fc7
Armin Richard Veres <armin.veres@proton.me>
2025-11-04 08:37:18 +0100

    build: make navigation simpler

--- .nvim.lua
+++
@@ -1,6 +0,0 @@
-local command = vim.list_extend(
-    require("plugins.lsp.settings.clangd").command_template,
-    { "--query-driver=/usr/bin/arm-none-eabi-gcc" }
-)
-
-vim.lsp.config("clangd", { cmd = command })
